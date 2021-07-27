858448ebae1b319d4b8758a02c4c711758ccf4ce
snowfirewolf <4399kevin11@gmail.com>
2021-07-27 15:35:05 +0800

    update

--- precache-manifest.5a606ea8f2bde0c0eae05f14db7c701e.js
+++ precache-manifest.5a606ea8f2bde0c0eae05f14db7c701e.js
@@ -1,19 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "aae87e0016377d216631",
-    "url": "/css/app.a7300603.css"
+    "revision": "6d1d7ee06059fd82d9c4",
+    "url": "/css/app.ebb4206c.css"
   },
   {
-    "revision": "d01c2b0a38441c1e5949",
+    "revision": "e2cef1bd4e97a0bc5c6b",
     "url": "/css/main.2d44f936.css"
+  },
+  {
+    "revision": "2c3c71275585b3017ef11838abcbc639",
+    "url": "/img/Image_2021_07_2021d-2021h.2c3c7127.png"
+  },
+  {
+    "revision": "023e09c4acbb9db4f27a0abcc99f18cc",
+    "url": "/img/Kevin_Zheng.023e09c4.jpg"
   },
   {
     "revision": "2a8b56ee0fd9acbbcb246d23d2705209",
     "url": "/img/background.2a8b56ee.webp"
   },
   {
-    "revision": "2c77ae3dfa8efede12947b37f035d2e1",
-    "url": "/img/me.2c77ae3d.jpg"
+    "revision": "5a8397ff061d6168af19ab0f595343e9",
+    "url": "/img/ipquery.5a8397ff.png"
   },
   {
     "revision": "bcadd0a24cece5324c162f8a1f167549",
@@ -24,20 +32,20 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/img/uipack.e220de89.png"
   },
   {
-    "revision": "4467f12187c8429fbea19b4bd9416523",
+    "revision": "880bc8a0678bc36e3180824c1f43a935",
     "url": "/index.html"
   },
   {
-    "revision": "aae87e0016377d216631",
-    "url": "/js/app.0defc75c.js"
+    "revision": "6d1d7ee06059fd82d9c4",
+    "url": "/js/app.bf5b2c06.js"
   },
   {
     "revision": "507bddf78d53c79d8202",
     "url": "/js/chunk-vendors.eeee7e0c.js"
   },
   {
-    "revision": "d01c2b0a38441c1e5949",
-    "url": "/js/main.478e808c.js"
+    "revision": "e2cef1bd4e97a0bc5c6b",
+    "url": "/js/main.690059d7.js"
   },
   {
     "revision": "8508d96416b4dd5897b89e9156399f4d",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2397a24aea456875526c8289175390be.js"
+  "/precache-manifest.5a606ea8f2bde0c0eae05f14db7c701e.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "snow-vue3"});
